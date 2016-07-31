7e1efb97f1e53cfb123b91104e5f96c185809633
Mrinal Sinha <themrinalsinha@gmail.com>
2016-07-31 16:39:19 +0530

    Add files via upload

---
+++ Binary Tree Class (STL-CPP)/assignment5.h
@@ -0,0 +1,41 @@
+#ifndef ASSIGNMENT5
+#define ASSIGNMENT5
+
+//Node Class
+class Node
+{
+	friend class binTree;
+	public:
+		Node(int& x, Node* l = 0, Node* r = 0)
+		{ data = x; left = l; right = r; }
+
+	private:
+		int data; 		// Holds current value.
+		Node* left; 	// Pointer to left tree.
+		Node* right; 	// Pointer to right tree.
+};
+
+
+
+class binTree {
+    public:
+        binTree();
+        virtual void insert( int );
+        unsigned height() const;
+        unsigned size() const;
+        void inorder( void(*)(int) );
+        void preorder( void(*)(int) );
+        void postorder( void(*)(int) );
+
+    protected:
+        Node* root;
+    private:
+        void insert( Node*&, int );
+        unsigned height( Node* ) const;
+        unsigned size( Node* ) const;
+	void inorder( Node*, void(*)(int) );
+	void preorder( Node*, void(*)(int) );
+	void postorder( Node*, void(*)(int) );
+};
+
+#endif
